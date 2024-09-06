8015c2c62e3b3f02437541354e08a172d313d013
thewebsite2023 <nhattiento2704@gmail.com>
2024-09-06 23:00:03 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,29 @@
+const listAccount = [{
+    username: '123', password: '123'
+  }, {
+    username: 'quantrivien', password: '123'
+  }, {
+    username: 'quantrivienweb', password: 'quantrivien'
+  }, {
+    username: 'service', password: 'service'
+  }];
+  let isLogin = !!localStorage.getItem('token');
+
+  function CheckLogin () {
+    if (isLogin) {
+      window.location.href = 'https://thewebsite2023.github.io/';
+    }
+  }
+
+  function Login () {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let checkLogin = listAccount.some(value => value.username === username && value.password === password);
+    if (checkLogin) {
+      localStorage.setItem('token', username);
+      isLogin = true;
+      CheckLogin();
+    } else {
+      alert('Tên đăng nhập hoặc mật khẩu sai!');
+    }
+  }
